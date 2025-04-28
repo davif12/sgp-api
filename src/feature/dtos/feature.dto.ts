229412b73cbb1410baa9dffe5e4c4658cc5e2533
davif12 <daviferreira1806@gmail.com>
2025-04-28 20:49:16 -0300

    feat(feature): CRUD de feature

---
+++ src/feature/dtos/feature.dto.ts
@@ -0,0 +1,13 @@
+import { Feature } from '../feature.entity';
+
+export class FeatureDto {
+  id: string;
+  description: string;
+
+  static fromEntity(f: Feature): FeatureDto {
+    const dto = new FeatureDto();
+    dto.id          = f.id;
+    dto.description = f.description;
+    return dto;
+  }
+}
